41a6fb35f9ac57a57a5776996b59f5d07ede1ab2
Ni3deshwal <ni3deswal@gmail.com>
2023-02-11 14:53:53 +0530

    interview question

---
+++ Interview Question/Java Script Interview/linkedin.js
@@ -0,0 +1,56 @@
+// let cat=Object.create({type:"lion"});
+// cat.size="large";
+//  let copyCat={...cat};
+//  cat.type="tiger";
+//  console.log(copyCat.type,copyCat.size);
+
+// var a=['dog','cat','hen'];
+// a[100]='fox';
+// console.log(a.length)
+
+// let obj={};
+// console.log(obj?.a.b)
+// console.log(obj.a?.b)
+// console.log(obj.?a.?b)
+// console.log(obj[a][b])
+
+// let r=["am","Bo","Ce","Co"]
+
+// r.splice(0,2);
+// console.log(r)
+
+// var obj;
+// console.log(obj)
+
+// console.log("I");
+// setTimeout(()=>{
+//     console.log("love")
+// },0)
+// console.log("Javascript !");
+
+// class Rain{
+//     static minrf=60;
+// }
+// let congo=new Rain();
+// Rain.minrf=80;
+// console.log(congo.minrf);
+
+// let rainfa=10;
+// let animals=0;
+// while(rainfa<13||animals<=2){
+//     rainfa++;
+//     animals +=2
+// }
+// console.log(animals)
+
+if(true)
+{
+    var first='you';
+}
+
+function fScope(){
+    var second="got this";
+}
+// fScope();
+// console.log(first)
+console.log(second);
